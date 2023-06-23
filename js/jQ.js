aa43bde8ef5ef50814d8c23b8214eb4fd62d59e0
kcy230426 <purple_galaxy@naver.com>
2023-06-23 18:09:05 +0900

    second

--- js/jQ.js
+++ js/jQ.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
+    $(window).scroll(function(){
+        // 스크롤을 하면
+        if($(window).scrollTop() > 80){
+            // 스크롤의 위치가 80보다 커지면
+            $("body").addClass("scroll");
+            // body에 scroll 클래스를 삽입해라.
+        }else{
+            $("body").removeClass("scroll")
+        };  })
+    $("#gnb li").click(function(){
+        $("#gnb li").removeClass("bolding")
+        $(this).addClass("bolding")
+    })
 })
